a6db3c05abdcd8d58729b452c79ce028cdd7def8
muneeb289 <muneeb289@yahoo.com>
2023-06-08 15:06:24 +0500

    test 11

--- Using_Bootstrap/general_script.js
+++ Using_Bootstrap/general_script.js
@@ -9,15 +9,28 @@ function collaps() {
     if (collapse) {
         document.getElementById("nav").setAttribute("class", "col1")
         document.getElementById("contarea").setAttribute("class", "col2")
+        console.log(123)
 
+        document.getElementById("div_home_icon").innerHTML = "<p>Home</p>";
+        document.getElementById("div_user_icon").innerHTML = "<p>Profile</p>";
+        document.getElementById("div_au_icon").innerHTML = "<p>About Us</p>";
+        document.getElementById("div_cu_icon").innerHTML = "<p>Contact Us</p>";
+        document.getElementById("div_lo_icon").innerHTML = "<p>Log Out</p>";
 
-        console.log(document.getElementById("home_icon").innerText(""))
         collapse = false;
     }
     else {
         document.getElementById("nav").setAttribute("class", "col3")
         document.getElementById("contarea").setAttribute("class", "col4")
+
+        document.getElementById("div_home_icon").innerHTML = `<img id="home_icon" class="nav_col_icon" src="./images/home.svg" alt="Home">`;
+        document.getElementById("div_user_icon").innerHTML = `<img id="user_icon" class="nav_col_icon" src="./images/user.svg" alt="Profile">`;
+        document.getElementById("div_au_icon").innerHTML = `<img id="au_icon" class="nav_col_icon" src="./images/au.svg" alt="About Us">`;
+        document.getElementById("div_cu_icon").innerHTML = `<img id="cu_icon" class="nav_col_icon" src="./images/cu.svg" alt="Contact Us">`;
+        document.getElementById("div_lo_icon").innerHTML = `<img id="lo_icon" class="nav_col_icon" src="./images/lo.svg" alt="Log Out">`;
+
         collapse = true;
+
     }
 
 }
