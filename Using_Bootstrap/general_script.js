931167b80ee70de24bd9d0c89467e926900466ef
muneeb289 <muneeb289@yahoo.com>
2023-06-08 12:32:06 +0500

    test 7

--- Using_Bootstrap/general_script.js
+++ Using_Bootstrap/general_script.js
@@ -1,11 +1,20 @@
 var sidebar = document.querySelector("col1");
 var mainContent = document.querySelector("col2");
+let collapse = true;
+
+document.getElementById("nav").addEventListener("click", collaps, false)
 
 
 function collaps() {
+    if (collapse) {
+        document.getElementById("nav").setAttribute("class", "col1")
+        document.getElementById("contarea").setAttribute("class", "col2")
+        collapse = false;
+    }
+    else {
+        document.getElementById("nav").setAttribute("class", "col3")
+        document.getElementById("contarea").setAttribute("class", "col4")
+        collapse = true;
+    }
 
-    console.log(123)
-
-    sidebar.classList.toggle("col3");
-    mainContent.classList.toggle("col4");
 }
