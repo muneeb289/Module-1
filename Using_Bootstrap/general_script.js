2eba91fe236de11316b7da55b2e34847036c0c87
muneeb289 <muneeb289@yahoo.com>
2023-06-08 10:29:21 +0500

    test 6

---
+++ Using_Bootstrap/general_script.js
@@ -0,0 +1,11 @@
+var sidebar = document.querySelector("col1");
+var mainContent = document.querySelector("col2");
+
+
+function collaps() {
+
+    console.log(123)
+
+    sidebar.classList.toggle("col3");
+    mainContent.classList.toggle("col4");
+}
